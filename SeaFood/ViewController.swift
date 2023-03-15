83cdb31f18df7fc82e7b3d6e5a09dc6849e9de37
Grzegorz Fraczek <gf@lnasoftware.com>
2023-03-15 15:53:57 +0100

    Image recognition implemented.

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
+        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         
     }
@@ -27,6 +27,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3(configuration: MLModelConfiguration()).model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {(request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Image processing failed")
+            }
+            
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
     
 }
@@ -38,6 +64,13 @@ extension ViewController: UIImagePickerControllerDelegate {
         
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             imageView.image = pickedImage
+            
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("Error converting image")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
