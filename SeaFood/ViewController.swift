f144c65fedc3a58ee3fd5db8298b9c55667533e1
Grzegorz Fraczek <gf@lnasoftware.com>
2023-03-15 14:25:29 +0100

    Inceptionv3 ML model added

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
