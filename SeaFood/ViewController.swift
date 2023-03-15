3112a4a5ddd2f04c0aeae0eae8d74681c8530b59
Grzegorz Fraczek <gf@lnasoftware.com>
2023-03-15 15:01:23 +0100

    ImagePicker added

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -18,10 +18,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
+}
+
+//MARK: - UIImagePickerControllerDelegate Methods
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = pickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
